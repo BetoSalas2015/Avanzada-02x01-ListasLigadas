7289604e5784351c5752250c57286dc31fa461d3
Roberto Salazar <roberto.salazar@upaep.mx>
2021-03-17 12:56:10 -0600

    Sesión 25 - 17/03/2021

--- ListasLigadas.c
+++ ListasLigadas.c
@@ -10,13 +10,32 @@ struct Nodo
 
 typedef struct Nodo nodo;
 
+nodo *raiz = NULL;					// creamos una lista vacía
+
+void insert(char dato)
+{
+	nodo *nuevo = NULL;				// creamos un apuntador auxiliar para la nueva area de memoria
+	nuevo = (nodo *) malloc( sizeof( nodo ) );		// creamos un nodo nuevo
+	if(nuevo == NULL)				// Error: no hay memoria disponible
+	{
+		printf("No hay memeoria disponible. \n");
+		return;						//  Se acabó!!
+	}
+	nuevo->info = dato;				// Preservamos la información
+	if(raiz == NULL)				// Es el primer nodo de la lista
+	{
+		raiz = nuevo;				// apuntamos raiz al nuevo nodo creado
+		nuevo->sig = NULL;			// Lo volvemos el ultimo nodo de la lista
+	}
+	else
+	{								// No es el primer nodo.
+
+	}
+}
 
 int main()
 {
-	int a;
-	nodo caja;
-	caja.info = 'c';
-	printf("el tamaño del struct Nodo es: %d bytes\n", sizeof( struct Nodo ) );
+
 	system("pause");
 	return 0;
 }
